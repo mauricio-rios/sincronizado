3c591793d19d2157c3de264b70e24994b9d169c8
MB-Rios <78772544+MB-Rios@users.noreply.github.com>
2021-02-08 21:38:13 -0600

    Update nav.php

--- nav.php
+++ nav.php
@@ -1,1 +1,8 @@
-
+<header>
+  <ol>
+  <li><a href="#">Home</a></li>
+  <li><a href="#">Quienes somos</a></li>
+  <li><a href="#">Nosotros</a></li>
+  <li><a href="#">Contacto</a></li>
+  </ol>
+</header>
